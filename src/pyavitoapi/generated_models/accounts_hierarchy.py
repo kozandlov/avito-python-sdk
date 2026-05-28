6f229b76cc02583da8dbecfcc3652b7c84dd6d82
kozandlov <49969338+kozandlov@users.noreply.github.com>
2026-05-28 06:54:18 +0000

    chore: auto-regenerate from Avito OpenAPI

--- src/pyavitoapi/generated_models/accounts_hierarchy.py
+++ src/pyavitoapi/generated_models/accounts_hierarchy.py
@@ -14,25 +14,43 @@ class AccountsHierarchyApiCheckAhUserV1Response(_BaseModel):
     is_company: bool = Field(default=None, alias='isCompany')
     is_employee: bool = Field(default=None, alias='isEmployee')
 
-class AccountsHierarchyApiGetEmployeesV1ResponseItemModel1(_BaseModel):
+class AccountsHierarchyApiCheckAhUserV2Response(_BaseModel):
+    avito_company_id: int | None = Field(default=None, alias='avitoCompanyId')
+    is_chief: bool = Field(default=None, alias='isChief')
+    is_company: bool = Field(default=None, alias='isCompany')
+    is_employee: bool = Field(default=None, alias='isEmployee')
+
+class AccountsHierarchyApiGetAhInfoV1ResponseCompanyModel1(_BaseModel):
+    is_company: bool = Field(alias='isCompany')
+
+class AccountsHierarchyApiGetAhInfoV1ResponseEmployeesItemModel2(_BaseModel):
+    avito_company_id: int | None = Field(default=None, alias='avitoCompanyId')
+    employee_id: int | None = Field(default=None, alias='employeeId')
+    is_chief: bool = Field(alias='isChief')
+
+class AccountsHierarchyApiGetAhInfoV1Response(_BaseModel):
+    company: AccountsHierarchyApiGetAhInfoV1ResponseCompanyModel1
+    employees: list[AccountsHierarchyApiGetAhInfoV1ResponseEmployeesItemModel2]
+
+class AccountsHierarchyApiGetEmployeesV1ResponseItemModel3(_BaseModel):
     email: str | None = None
     employee_id: int = Field(alias='employeeId')
     is_chief: bool = Field(default=None, alias='isChief')
     name: str | None = None
     phones: list[str] = None
 
-class AccountsHierarchyApiGetEmployeesV1Response(RootModel[list[AccountsHierarchyApiGetEmployeesV1ResponseItemModel1]]):
+class AccountsHierarchyApiGetEmployeesV1Response(RootModel[list[AccountsHierarchyApiGetEmployeesV1ResponseItemModel3]]):
     pass
 
-class AccountsHierarchyApiListCompanyPhonesV1ResponseResultModel2(_BaseModel):
+class AccountsHierarchyApiListCompanyPhonesV1ResponseResultModel4(_BaseModel):
     cursor: str | None = None
     phones: list[str] = None
 
 class AccountsHierarchyApiListCompanyPhonesV1Response(_BaseModel):
-    result: AccountsHierarchyApiListCompanyPhonesV1ResponseResultModel2 = None
+    result: AccountsHierarchyApiListCompanyPhonesV1ResponseResultModel4 = None
 
 class AccountsHierarchyApiListItemsByEmployeeIdV1Response(_BaseModel):
     has_next: bool = Field(alias='hasNext')
     items: list[int]
 
-__all__ = ['AccountsHierarchyApiCheckAhUserV1Response', 'AccountsHierarchyApiGetEmployeesV1ResponseItemModel1', 'AccountsHierarchyApiGetEmployeesV1Response', 'AccountsHierarchyApiListCompanyPhonesV1ResponseResultModel2', 'AccountsHierarchyApiListCompanyPhonesV1Response', 'AccountsHierarchyApiListItemsByEmployeeIdV1Response']
+__all__ = ['AccountsHierarchyApiCheckAhUserV1Response', 'AccountsHierarchyApiCheckAhUserV2Response', 'AccountsHierarchyApiGetAhInfoV1ResponseCompanyModel1', 'AccountsHierarchyApiGetAhInfoV1ResponseEmployeesItemModel2', 'AccountsHierarchyApiGetAhInfoV1Response', 'AccountsHierarchyApiGetEmployeesV1ResponseItemModel3', 'AccountsHierarchyApiGetEmployeesV1Response', 'AccountsHierarchyApiListCompanyPhonesV1ResponseResultModel4', 'AccountsHierarchyApiListCompanyPhonesV1Response', 'AccountsHierarchyApiListItemsByEmployeeIdV1Response']
